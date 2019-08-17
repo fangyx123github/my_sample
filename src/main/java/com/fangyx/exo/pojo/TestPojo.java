b750d4e9c9a7949bfae1b2c5e7876b66e14ef547
fangyx <fangyx_1992@sina.com>
2019-08-17 23:34:22 +0800

    测试分支

---
+++ src/main/java/com/fangyx/exo/pojo/TestPojo.java
@@ -0,0 +1,13 @@
+package com.fangyx.exo.pojo;
+
+/**
+ * @Author: Fangyx
+ * @Description:
+ * @CreateDate: 2019/8/17 23:33
+ * @UpdateUser:
+ * @UpdateDate: 2019/8/17 23:33
+ * @UpdateRemark: 更新说明
+ * @Version: 1.0
+ **/
+public class TestPojo {
+}
