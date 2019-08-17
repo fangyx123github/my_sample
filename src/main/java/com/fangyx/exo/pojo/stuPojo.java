d55ad5f2c91881e4188962b91506daaaddaa237d
fangyx <fangyx_1992@sina.com>
2019-08-17 23:49:57 +0800

    测试分支-1

---
+++ src/main/java/com/fangyx/exo/pojo/stuPojo.java
@@ -0,0 +1,13 @@
+package com.fangyx.exo.pojo;
+
+/**
+ * @Author: Fangyx
+ * @Description:
+ * @CreateDate: 2019/8/17 23:49
+ * @UpdateUser:
+ * @UpdateDate: 2019/8/17 23:49
+ * @UpdateRemark: 更新说明
+ * @Version: 1.0
+ **/
+public class stuPojo {
+}
